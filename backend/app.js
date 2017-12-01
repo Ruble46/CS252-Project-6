094d6c69e11d19360992314037a192744b968e77
Brett <beiffert@purdue.edu>
2017-11-30 20:25:02 -0500

    added dependencies and basic mySQL server

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+// app.js
+const mysql = require('mysql');
+
+// First you need to create a connection to the db
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'test1test',
+  database: 'mysql'
+});
+
+con.connect((err) => {
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+con.query('SELECT * FROM employees', (err,rows) => {
+  if(err) throw err;
+
+  console.log('Data received from Db:\n');
+  console.log(rows);
+  rows.forEach( (row) => { 
+  	console.log(`${row.name} is in ${row.location}`); 
+  });
+});
+
+con.end((err) => {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
+
+
+
+
+
